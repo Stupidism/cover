e139b14470267057815f8e53edfb91c5aea31cfc
utf8please <___@sjtu.edu.cn>
2015-07-05 15:47:15 +0800

    Implement token-based autoLogin.

--- server/assets/js/cloud-video/controllers/master.js
+++ server/assets/js/cloud-video/controllers/master.js
@@ -2,38 +2,55 @@ angular.module('cover').controller('MasterCtrl', function ($scope, $q, $modal, $
   $scope.currentUser = null;
 
   var loginPromise;
+  $scope.autoLogin = function () {
+    var accessToken = localStorage['accessToken'];
+    if (!accessToken) return $q.reject();
+    return loginPromise = $http.get('/api/account/current', {headers: {
+      'Access-Token': accessToken
+    }}).then(function (res) {
+      $scope.currentUser = JsonApiOrg.parse(res.data);
+      $scope.currentUser.$token = accessToken;
+      return $scope.currentUser;
+    }, function (e) {
+      delete localStorage['accessToken'];
+      return $q.reject(e);
+    }).catch(function (e) {
+      loginPromise = null;
+      return $q.reject(e);
+    }) || $q.reject();
+  };
+  $scope.autoLogin();
   $scope.login = function () {
     if ($scope.currentUser)
       return $q.when($scope.currentUser);
-    loginPromise = null;
-    if (!loginPromise || loginPromise.$rejected) {
+    if (!loginPromise) {
       loginPromise = $modal.open({
           templateUrl : '/assets/partials/login.html',
           backdrop : 'static',
           keyboard : false,
           size : 'sm',
-        }).result.then(function (user) {
-          user.role = 1;
-          return $http.post('/api/account/login', user)
-          .then(function (res) {
-            $scope.currentUser = JsonApiOrg.parse(res.data.user);
-            $scope.currentUser.$token = res.data.token;
-            console.log($scope.currentUser);
-          }, function (res) {
-            console.info(res);
-            if(res.status==403)
-              alert("用户名或密码错误");
-          });
-        }, function (e) {
-          console.info(e);
+        }).result.catch(function (e) {
           if (e === 'register')
-            return $scope.register().catch (function (e) {
-              loginPromise = null;
-              return $q.reject(e);
-            });
-          loginPromise = null;
+            return $scope.register();
           return $q.reject(e);
+        }).then(function (user) {
+          user.role = 1;
+          return $http.post('/api/account/login', user).catch(function (res) {
+            if (res.status === 403 || res.status === 404) {
+              alert('用户名或密码错误！')
+            }
+            return $q.reject(res);
+          });
+        }).then(function (res) {
+          $scope.currentUser = JsonApiOrg.parse(res.data.user);
+          $scope.currentUser.$token = res.data.token;
+          localStorage['accessToken'] = $scope.currentUser.$token;
+          console.log($scope.currentUser);
+          return $scope.currentUser;
         });
+      loginPromise.catch(function (e) {
+        loginPromise = null;
+      });
     }
     return loginPromise || $q.reject();
   };
@@ -71,6 +88,7 @@ angular.module('cover').controller('MasterCtrl', function ($scope, $q, $modal, $
     $http.post("/api/account/logout", {}, {headers: {
       'Access-Token': $scope.currentUser.$token,
     }}).then(function () {
+      delete localStorage['accessToken'];
       window.location.reload();
     });
   };
