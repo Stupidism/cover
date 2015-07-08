64c8ada37c8ed5c9c381b3065f0ee2463dfbfb7c
12sunyan <sunyan940317@163.com>
2015-07-08 15:18:23 +0800

    增加问题与讨论及测试管理两个页面
    如题

--- server/assets/js/cloud-video/app.js
+++ server/assets/js/cloud-video/app.js
@@ -46,6 +46,16 @@ angular.module('cover', [
     url: '/subjects/:subject',
     controller: 'SubjectDetailsCtrl',
     templateUrl: 'assets/partials/subject_details.html',
+  }).state('courseManage.problems', {
+    url: '/problems',
+    templateUrl: 'assets/partials/problem_list.html',
+  }).state('courseManage.problemDetails', {
+    url: '/problems/:problems',
+    controller: 'ProblemDetailsCtrl',
+    templateUrl: 'assets/partials/problem_details.html',
+  }).state('courseManage.exercises', {
+    url: '/exercises',
+    templateUrl: 'assets/partials/exercise_list.html',
   }).state('courseManage.classes', {
     url: '/classes',
     templateUrl: 'assets/partials/class_list.html',
