c6fd5c72e039c55ce5a8b92ae5e9f4b25c88e444
12sunyan <sunyan940317@163.com>
2015-07-27 12:23:57 +0800

    修复显示所有课程中的bug

--- student/assets/js/cloud-video/controllers/course_all.js
+++ student/assets/js/cloud-video/controllers/course_all.js
@@ -75,4 +75,10 @@ function ($scope,$http,$timeout, Restangular,JsonApiOrg, $state,$modal) {
         })
       });
   };
+  $scope.courseEnum={
+    1:'公共基础课',
+    2:'专业核心课',
+    3:'实训课',
+    8:'模板课',
+  }
 })
