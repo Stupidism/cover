b7d5d8169ac38eeac7b56e99ea0c8b550a78adbc
fitailsh <derrick65536@gmail.com>
2015-08-06 16:25:15 +0800

    student's UI update

--- student/assets/js/cloud-video/controllers/course_outline.js
+++ student/assets/js/cloud-video/controllers/course_outline.js
@@ -1,3 +1,6 @@
-angular.module('student').controller('CourseOutlineCtrl', function ($scope, Restangular, $state, JsonApiOrg) {
-  $scope.courseoutline = $scope.course.$related.courseoutline;
+angular.module('student').controller('CourseOutlineCtrl', function ($scope, Restangular, $state,$stateParams,JsonApiOrg) {
+   $scope.fetchCourse = Restangular.one('courses', $stateParams.course).get();
+   $scope.fetchCourse.then(function (course) {
+     $scope.courseoutline = course.$related.courseoutline;
+   });
 });
