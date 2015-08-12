997d674771202f238bb8005a8e95ac3efe16f084
loserstar <490195824@qq.com>
2015-08-12 15:47:37 +0800

    add search keyword to resource manage page

--- SuperManage/scripts/controllers/resource_manage.js
+++ SuperManage/scripts/controllers/resource_manage.js
@@ -4,6 +4,7 @@ function ($scope, $http, $state, $modal, $timeout, Restangular, $rootScope) {
   $rootScope.pageTitle = "资源管理";
   $scope.login().then(function (){
     $scope.nowPage = 1;
+    $scope.keyword = "";
     http(1);
     $scope.delete = function(resource)
     {
@@ -29,11 +30,15 @@ function ($scope, $http, $state, $modal, $timeout, Restangular, $rootScope) {
       $scope.nowPage++;
       http($scope.nowPage);
     };
+    $scope.search = function()
+    {
+      http($scope.nowPage);
+    }
     function http(now)
     {
       var reqGET = {
         method: 'GET',
-        url: '/api/resources?page='+now.toString()+'&size=10',
+        url: '/api/resources?page='+now.toString()+'&size=10&keyword='+$scope.keyword,
         headers: {
           'Access-Token': $scope.currentUser.$token,
         },
