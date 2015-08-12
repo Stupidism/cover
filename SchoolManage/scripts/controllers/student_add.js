beab3a02b31ad8d0888831911beebe5b00b4f128
loserstar <490195824@qq.com>
2015-08-12 11:00:36 +0800

    学生添加页面默认选择选项避免不选

--- SchoolManage/scripts/controllers/student_add.js
+++ SchoolManage/scripts/controllers/student_add.js
@@ -4,7 +4,6 @@ function ($scope, $http, $state, $timeout, Restangular, $rootScope) {
   $rootScope.pageTitle = "学生管理 - 添加学生";
   $scope.login().then(function (){
   $scope.school = $scope.currentUser.$related.school;
-  $scope.majors = Restangular.all('schools/'+$scope.school.$id.toString()+'/majors').getList().$object;
   var original;
   return $scope.user = {
       $type: "student",
@@ -15,27 +14,25 @@ function ($scope, $http, $state, $timeout, Restangular, $rootScope) {
       password: "",
       email: "",
       code: "",
-      gender:"",
+      gender: "",
       truename: "",
       department: "",
-      title: "",
       phone: "",
       credit: "",
       level: "",
       address: "",
-      gendername:""
+      gendername: ""
   },
   $scope.showInfoOnSubmit = !1,
+  $scope.user.gendername = "男",
+  Restangular.all('schools/'+$scope.school.$id.toString()+'/majors').getList().then(function(majors) {
+    $scope.majors = majors;
+    $scope.major = $scope.majors[0];
+  }),
   original = angular.copy($scope.user),
   $scope.revert = function() {
       return $scope.user = angular.copy(original),
       $scope.form_signin.$setPristine()
-  },
-  $scope.canRevert = function() {
-      return ! angular.equals($scope.user, original) || !$scope.form_signin.$pristine
-  },
-  $scope.canSubmit = function() {
-      return $scope.form_signin.$valid && !angular.equals($scope.user, original)
   },
   $scope.submitForm = function() {
     $scope.user.$relationships.major = {data:$scope.major.$asLink()};
